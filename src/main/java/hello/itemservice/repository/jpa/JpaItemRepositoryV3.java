7cffa5ee21fce155e4e35ec9e7830991edd4fa83
refresh88 <115513248+refresh88@users.noreply.github.com>
2023-07-08 01:22:31 +0900

    section7 Querydsl

---
+++ src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV3.java
@@ -0,0 +1,110 @@
+package hello.itemservice.repository.jpa;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import hello.itemservice.domain.Item;
+import hello.itemservice.domain.QItem;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import org.springframework.data.jpa.repository.query.JpaQueryCreator;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+import static hello.itemservice.domain.QItem.*;
+
+@Repository
+@Transactional
+public class JpaItemRepositoryV3 implements ItemRepository {
+
+    /**
+     * Querydsl을 사용하려면 'JPAQueryFactory' 가 필요하다.
+     * 'JPAQueryFactory'는 JPA 쿼리인 JPQL을 만들기 때문에 `EntityManager` 가 필요하다.
+     */
+    private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public JpaItemRepositoryV3(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public Item save(Item item) {
+        em.persist(item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = em.find(Item.class, itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        Item item = em.find(Item.class, id);
+        return Optional.ofNullable(item);
+    }
+
+    public List<Item> findAllOld(ItemSearchCond cond) {
+
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+//        QItem item = new QItem("i"); // i 는 alias
+        //QItem 가보면 public static final QItem item = new QItem("item"); 이런게 있음 이걸 걍 쓰면됨.
+        // QItme.item 은 item으로 축약 가능.
+        QItem item = QItem.item;
+        BooleanBuilder builder = new BooleanBuilder();
+        if(StringUtils.hasText(itemName)) {
+            builder.and(item.itemName.like("%" + itemName + "%"));
+        }
+        if(maxPrice != null) {
+            builder.and(item.price.loe(maxPrice)); // loe는 작거나 같다를 의미. 자바에서 <>= 요게 안됨..?
+        }
+
+        List<Item> result = query
+                .select(item)
+                .from(item)
+                .where(builder)
+                .fetch();
+
+        return result;
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        return query
+                .select(item)
+                .from(item)
+                .where(LikeItemName(itemName), maxPrice(maxPrice))
+                .fetch();
+    }
+
+    private BooleanExpression maxPrice(Integer maxPrice) {
+        if(maxPrice != null) {
+            return item.price.loe(maxPrice);
+        }
+        return null;
+    }
+
+    private BooleanExpression LikeItemName(String itemName) {
+        if(StringUtils.hasText(itemName)) {
+            return item.itemName.like("%" + itemName + "%");
+        }
+        return null;
+    }
+}
